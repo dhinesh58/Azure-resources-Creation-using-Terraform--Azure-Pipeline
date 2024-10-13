ff83e4b4cde988ff2403f360b205e78b20add2cc
Dhinesh <157783841+dhinesh58@users.noreply.github.com>
2024-10-13 21:08:44 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,7 @@
 provider "azurerm" {
-    features {}
+client_id= "${arm_client_id}"
+client_secret= "{arm_client_secret}"
+tenant_id= "{arm_tenant_id}"
+subscription_id="{arm_subscription_id}"
+features {}
 }
